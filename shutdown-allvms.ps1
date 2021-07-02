3c87beeda24eaa24c586d94b67c6f99c87c965a1
floalt <flo.alt@fa-netz.de>
2021-07-02 23:13:59 +0200

    shutdown-allvms.ps1 modified

--- shutdown-allvms.ps1
+++ shutdown-allvms.ps1
@@ -3,5 +3,5 @@ Get-VM | where {($_.State -eq 'Running')} | Stop-VM
 
 #sleep 120
 
-# Domaincontroller starten
-Get-VM | where {($_.State -eq 'Running') -and ($_.Name -like "*-dc*")} | Start-VM
+# Domaincontroller herunterfahren
+Get-VM | where {($_.State -eq 'Running') -and ($_.Name -like "*-dc*")} | Stop-VM
